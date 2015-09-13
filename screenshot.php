ef38357e76a9d09a80950fbc37237e1cfa497d52
Jannick Fahlbusch <git@jf-projects.de>
2015-09-13 22:43:29 +0200

    Complete the information for the help-plugin

--- screenshot.php
+++ screenshot.php
@@ -1,6 +1,9 @@
 <?php
 class screenshot extends Script
 {
+	protected $helpMessage = "'screenshot URL'\n";
+	protected $description = 'Returns a screenshot of the given URL';
+
 	public function run()
 	{
 		return $this->send('http://screenshotlayer.com/php_helper_scripts/scl_api.php?url=' . $this->matches[1], 'image');
